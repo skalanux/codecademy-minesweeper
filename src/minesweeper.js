b698b798a34b63784c3ec4ff53f912659484faec
Juan Manuel Schillaci <jmschillaci@gmail.com>
2017-08-31 22:25:00 -0300

    WIP

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,15 +1,39 @@
 const printBoard = board => {
     console.log('Current Board:');
-    console.log(board[0].join(' | '));
-    console.log(board[1].join(' | '));
-    console.log(board[2].join(' | '));
-
+    console.log(board.map(row => row.join('|')).join('\n'));
+    
+    
 };
-let board = [];
-board = [[' ', ' ', ' '], 
-        [' ', ' ', ' '],
-        [' ', ' ', ' ']];
-printBoard(board);
-board[0][1] = 1;
-board[2][2] = 'B';
-printBoard(board);
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+    let board = [];
+    for (let rowIndex=0; rowIndex < numberOfRows.length; rowIndex++){
+        let row = [];
+        for (let colIndex=0; colIndex < numberOfColumns.length; colIndex++){
+            row.push(' ');
+        }
+        board.push(row);
+    }
+    return board;
+};
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    let board = [];
+    for (let rowIndex=0; rowIndex < numberOfRows.length; rowIndex++){
+        let row = [];
+        for (let colIndex=0; colIndex < numberOfColumns.length; colIndex++){
+            row.push(null);
+        }
+        board.push(row);
+    }
+    let numberOfBombsPlaced = 0;
+
+    while (numberOfBombsPlaced<numberOfBombs){
+        let randomRowIndex;
+        randomRowIndex = Math.floor(Math.random()*numberOfRows);
+        randomColIndex = Math.floor(Math.random()*numberOfColumns);
+        // Fixme: Now it can placed bombs on the same place as before
+        board[randomRowIndex, randomColIndex] = 'B';
+        numberOfBombsPlaced++;
+    }
+    return board;
+}
