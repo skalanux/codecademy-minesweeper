756633cce671b3c624284990ad123d60c3842e05
Juan Manuel Schillaci <jmschillaci@gmail.com>
2017-09-02 14:29:19 -0300

    Day 10,11

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,15 +1,14 @@
 const printBoard = board => {
-    console.log('Current Board:');
-    console.log(board.map(row => row.join('|')).join('\n'));
-    
-    
+    console.log(board.map(row => row.join(' | ')).join('\n'));
 };
+
 const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
     let board = [];
-    for (let rowIndex=0; rowIndex < numberOfRows.length; rowIndex++){
+    console.log(numberOfRows);
+    for (let rowIndex=0; rowIndex < numberOfRows; rowIndex++){
         let row = [];
-        for (let colIndex=0; colIndex < numberOfColumns.length; colIndex++){
-            row.push(' ');
+        for (let colIndex=0; colIndex < numberOfColumns; colIndex++){
+            row.push('');
         }
         board.push(row);
     }
@@ -18,22 +17,27 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
 
 const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
     let board = [];
-    for (let rowIndex=0; rowIndex < numberOfRows.length; rowIndex++){
+    for (let rowIndex=0; rowIndex < numberOfRows; rowIndex++){
         let row = [];
-        for (let colIndex=0; colIndex < numberOfColumns.length; colIndex++){
+        for (let colIndex=0; colIndex < numberOfColumns; colIndex++){
             row.push(null);
         }
         board.push(row);
     }
     let numberOfBombsPlaced = 0;
-
     while (numberOfBombsPlaced<numberOfBombs){
-        let randomRowIndex;
-        randomRowIndex = Math.floor(Math.random()*numberOfRows);
-        randomColIndex = Math.floor(Math.random()*numberOfColumns);
+        let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+        let randomColIndex = Math.floor(Math.random() * numberOfColumns);
         // Fixme: Now it can placed bombs on the same place as before
-        board[randomRowIndex, randomColIndex] = 'B';
+        //"Antes de las bombas" +  console.log(`Putting bomb on row: ${randomRowIndex} column: ${randomColIndex}`)
+        board[randomRowIndex][randomColIndex] = 'B';
         numberOfBombsPlaced++;
     }
     return board;
 }
+let playerBoard = generatePlayerBoard(3, 4);
+let bombBoard = generateBombBoard(3, 4, 5);
+console.log('Player Board:');
+printBoard(playerBoard);
+console.log('Bomb Board:');
+printBoard(bombBoard);
